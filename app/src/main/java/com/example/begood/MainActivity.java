ba3e8a718c456fb8b6f397a40ba75ac4441b84dd
sapir.holzman <sapir.holzman@gmail.com>
2021-01-16 17:16:37 +0200

    change font size and add onclick login function

--- app/src/main/java/com/example/begood/MainActivity.java
+++ app/src/main/java/com/example/begood/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.begood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginBtn = findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Tag", "login");
+            }
+        });
     }
 }
