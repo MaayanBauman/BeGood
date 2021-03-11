0803e40cbae843ceaded5c265ff90e055150e450
maayanb80 <maayanb80@gmail.com>
2021-03-11 13:57:39 +0200

    remove comment

--- app/src/main/java/com/example/begood/models/AppLocalDb.java
+++ app/src/main/java/com/example/begood/models/AppLocalDb.java
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase;
 
 import com.example.begood.MyApplication;
 
-@Database(entities = { Post.class, User.class }, version = 3)
+@Database(entities = { Post.class, User.class }, version = 4)
 abstract class AppLocalDbRepository extends RoomDatabase {
     public abstract PostDao postDao();
 }
